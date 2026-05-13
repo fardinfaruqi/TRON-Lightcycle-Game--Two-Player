87649d06d4ec2bde1f7f15a04625765ba73e201a
fardinfaruqi <fardinfaruqi35@gmail.com>
2026-05-14 02:12:59 +0600

    Created Player A. It moves forward every frame.

--- Tron_Lightcycle.py
+++ Tron_Lightcycle.py
@@ -7,6 +7,17 @@ screen.bgcolor("black")
 screen.title("TRON Lightcycle")
 screen.tracer(0)        # disable auto-animation
 
+# --- Player A ---
+a = turtle.Turtle()
+a.color("cyan")
+a.pensize(3)
+a.speed(0)
+a.penup()
+a.goto(-160, 0)
+a.setheading(0)     # face right
+a.pendown()         # start drawing trail
+
 while True:
+    a.forward(5)        # move 5 px every frame
     screen.update()     # manually refresh
     time.sleep(0.013)   # ~75 fps
